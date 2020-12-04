f3b3dc4761973bcc30769decd4b2e92a996f69d0
Pierre-Olivier <pierreolivier.morin@outlook.com>
2020-12-04 01:46:57 -0500

    update on vue.add()

--- pokpokmeuhv2/src/main.js
+++ pokpokmeuhv2/src/main.js
@@ -1,9 +1,8 @@
 
 import App from './App.vue'
-import LoadScript from 'vue-plugin-load-script';
 import Vue from "vue";
 
-Vue.use(LoadScript);
+
 new Vue({
     render: h=>h(App)
 }).$mount('#app')
