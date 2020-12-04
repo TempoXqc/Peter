6e8b0a8eada44106bbfcf44be25f3d5ebd5eaff1
Peter <peterdescamps.quebec@gmail.com>
2020-12-04 11:34:06 -0500

    test variable channel

--- pokpokmeuhv2/src/components/vues/Twitch.vue
+++ pokpokmeuhv2/src/components/vues/Twitch.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="main">
+    {{ adresse() }}
     <div class="">
       <img src="../../assets/logo.png" class="img-logo" alt="" />
     </div>
     <div class="box-twitch">
-      <div style="margin: 0 25px; width:35%">
+      <div class="bloc-box-twitch">
         <h2>TKH</h2>
         <iframe
           :src="test"
@@ -23,7 +24,7 @@
         ></iframe>
       </div>
 
-      <div style="margin: 0 25px; width:35%">
+      <div class="bloc-box-twitch">
         <h2>TKH</h2>
         <iframe
           :src="test"
@@ -49,8 +50,7 @@
 export default {
   data() {
     return {
-      test:
-        "https://player.twitch.tv/?channel=oonolive&parent=pokpokmeuh.s3.us-east-2.amazonaws.com",
+      test: "https://player.twitch.tv/?channel=oonolive&parent=localhost",
       melo: "locklear",
       tkh: "kinggeorge",
       baba: "ambition_baba",
@@ -58,7 +58,21 @@ export default {
       width: "100%",
       height: "350",
       volume: "0",
+      baseUrl: "https://player.twitch.tv/",
+      channel: "/?channel=",
     };
+  },
+  computed: {
+    adresse(channel) {
+      return (
+        this.baseUrl +
+        "/?channel=" +
+        channel +
+        +"&parent=" +
+        this.melo +
+        window.location.host
+      );
+    },
   },
 };
 </script>
@@ -68,7 +82,6 @@ export default {
   width: 5%;
   border-radius: 150px;
 }
-
 h2 {
   margin: 0;
   color: white;
@@ -79,6 +92,10 @@ h2 {
   justify-content: center;
   width: 100%;
   box-sizing: border-box;
+}
+.bloc-box-twitch {
+  margin: 0 25px;
+  width: 35%;
 }
 .box-video {
   width: 100% !important;
