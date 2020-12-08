905ba87f274a3c6eec2f96211d8fe2d615e1f18c
AlexandreBear <alexandreperron3@gmail.com>
2020-12-08 18:27:13 -0500

    fixed

--- pokpokmeuhv2/src/components/subComponents/NavigationDrawerStreamer.vue
+++ pokpokmeuhv2/src/components/subComponents/NavigationDrawerStreamer.vue
@@ -11,14 +11,9 @@
     <v-list dense nav>
       <v-list-item
         v-for="item in getStreamers"
-        :key="item.title"
-        link
-        :to="item.route"
+        :key="item"
       >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        <v-list-item-title>{{ item }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
@@ -26,39 +21,6 @@
 <script>
 import {mapGetters} from "vuex"
 export default {
-  data() {
-    return {
-      items: [
-        {
-          title: "Général",
-          icon: "mdi-view-dashboard",
-          route: "/admin/general",
-        },
-        {
-          title: "Utilisateurs",
-          icon: "mdi-clipboard-account",
-          route: "/admin/utilisateurs",
-        },
-        {
-          title: "Projets",
-          icon: "mdi-folder",
-          route: "/admin/admin-projects",
-        },
-        { title: "Rôles", icon: "mdi-layers-triple", route: "/admin/roles" },
-        {
-          title: "Azure",
-          icon: "mdi-microsoft-azure-devops",
-          route: "/admin/azure",
-        },
-        {
-          title: "Clients",
-          icon: "mdi-account-cash",
-          route: "/admin/clients",
-        },
-      ],
-      right: null,
-    };
-  },
   computed: {
       ...mapGetters(["getStreamers"])
   },
